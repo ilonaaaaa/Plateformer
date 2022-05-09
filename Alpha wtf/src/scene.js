220d1f91c10efe8190e367108051f9cf66c9a45b
ilonaaaaa <92451528+ilonaaaaa@users.noreply.github.com>
2022-05-09 09:52:22 +0200

    kms

--- Alpha wtf/src/scene.js
+++ Alpha wtf/src/scene.js
@@ -1,8 +1,6 @@
 class scene extends Phaser.Scene {
 
     preload() {
-
-
         // At last image must be loaded with its JSON
         this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
         this.load.image('tiles', 'assets/tilesets/tilesheetFT.png');
@@ -12,13 +10,9 @@ class scene extends Phaser.Scene {
     }
 
     create() {
-
-        const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
-        backgroundImage.setScale(5, 1.5)
         this.map = this.make.tilemap({key: 'map'});
         this.tileset = this.map.addTilesetImage('tilesheetFT','tiles')
         this.cursors = this.input.keyboard.createCursorKeys();
-
 
         this.player = new Player(this)
 
