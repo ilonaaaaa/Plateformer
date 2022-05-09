655e0039ed6d1d2229764aa0d2d41642745c7db2
ilonaaaaa <92451528+ilonaaaaa@users.noreply.github.com>
2022-05-09 14:10:12 +0200

    maybe

--- Alpha wtf/src/main.js
+++ Alpha wtf/src/main.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.AUTO,
     parent: 'game',
     width: 1920,
-    heigth: 1080,
+    heigth: 1088,
     scale: {
         mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH
