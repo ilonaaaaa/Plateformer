27083ec635e2dcb721d05d0ed205c79a8c6cd3a8
ilonaaaaa <92451528+ilonaaaaa@users.noreply.github.com>
2022-05-09 09:43:13 +0200

    aled

--- Alpha wtf/src/scene.js
+++ Alpha wtf/src/scene.js
@@ -15,12 +15,8 @@ class scene extends Phaser.Scene {
 
         const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
         backgroundImage.setScale(5, 1.5)
-        const map = this.make.tilemap({key: 'map'});
-
-        const tileset = map.addTilesetImage('blockout', 'tiles');
-        this.platforms = map.createStaticLayer('Sol', tileset);
-
-        
+        this.map = this.make.tilemap({key: 'map'});
+        this.tileset = this.map.addTilesetImage('tilesheetFT','tiles')
         this.cursors = this.input.keyboard.createCursorKeys();
 
 
