05b69b2afd0223abf7995b795126c120d81be0e2
ilonaaaaa <92451528+ilonaaaaa@users.noreply.github.com>
2022-05-09 13:49:21 +0200

    mourir

--- Alpha wtf/src/scene.js
+++ Alpha wtf/src/scene.js
@@ -1,17 +1,24 @@
 class scene extends Phaser.Scene {
 
+    constructor(key) {
+        super(key);
+    }
+
     preload() {
-        // At last image must be loaded with its JSON
         this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
         this.load.image('tiles', 'assets/tilesets/tilesheetFT.png');
-
-        // Load the export Tiled JSON
-        this.load.tilemapTiledJSON('map', 'assets/tilemaps/blouckout.json');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/blockout.json');
     }
 
     create() {
-        this.map = this.make.tilemap({key: 'map'});
-        this.tileset = this.map.addTilesetImage('tilesheetFT','tiles')
+
+        this.map = this.make.tilemap({ key: 'map' });
+        this.tileset = this.map.addTilesetImage('tilesheetFT', 'tiles');
+
+        this.sol = this.map.createLayer('decor2', this.tileset, 0, 0);
+        this.shiny = this.map.createLayer('shiny', this.tileset, 0, 0);
+
+
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.player = new Player(this)
