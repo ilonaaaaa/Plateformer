91ae4f1a82e7d58bc4a2f86b8616ec1d9c4d0073
ilonaaaaa <92451528+ilonaaaaa@users.noreply.github.com>
2022-05-09 09:13:35 +0200

    k

--- Alpha wtf/src/scene.js
+++ Alpha wtf/src/scene.js
@@ -2,22 +2,22 @@ class scene extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/images/background.png');
-        this.load.image('spike', 'assets/images/spike.png');
+
         // At last image must be loaded with its JSON
         this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
-        this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+        this.load.image('tiles', 'assets/tilesets/tilesheetFT.png');
 
         // Load the export Tiled JSON
-        this.load.tilemapTiledJSON('map', 'assets/tilemaps/Alpha1.json');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/blockout.json');
     }
 
     create() {
 
         const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
-        backgroundImage.setScale(1, 0.8);
+        backgroundImage.setScale(5, 0.1)
         const map = this.make.tilemap({key: 'map'});
 
-        const tileset = map.addTilesetImage('Alpha_test1', 'tiles');
+        const tileset = map.addTilesetImage('Alpha_1', 'tiles');
         this.platforms = map.createStaticLayer('Sol', tileset);
 
         this.platforms.setCollisionByExclusion(-1, true);
