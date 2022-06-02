a3f67bac756c4ac340e77788b14bc69ee55ff77d
solarinfire <45672300+solarinfireoff@users.noreply.github.com>
2022-06-02 11:02:52 +0200

    you are phoenix

--- src/UI.js
+++ src/UI.js
@@ -15,7 +15,7 @@ class UI extends Phaser.Scene {
         this.add.sprite(50,50,'fragUi')
             .setDisplaySize(40,40);
 
-        this.count1 = this.add.text(100 ,50 , window.objet_fragment + '/9',{
+        this.count1 = this.add.text(120 ,50 , objet_fragment + '/9',{
             color: '#ffffff',
             fontFamily: 'cursive',
             fontSize : 40
@@ -24,25 +24,24 @@ class UI extends Phaser.Scene {
             .setAlpha(1)
 
 
-        this.count2 = this.add.text(100 ,50 , window.objet_fragment + '/9, end wall breakable !',{
+        this.count2 = this.add.text(200 ,100 , 'End wall breakable !',{
             color: '#ffffff',
             fontFamily: 'cursive',
             fontSize : 40
         })
             .setOrigin(0.5)
             .setAlpha(1)
+
     }
 
     update(){
-        this.count1.setText(Math.round(window.objet_fragment));
-        this.count2.setText(Math.round(window.objet_fragment));
+        this.count1.setText(Math.round(window.objet_fragment) + '/9');
+        this.count2.setText('End wall breakable !');
 
         if (window.objet_fragment<7){
-            this.count1.visible = true;
             this.count2.visible = false;
         }
         else if (window.objet_fragment>=7){
-            this.count1.visible = false;
             this.count2.visible = true;
         }
     }
